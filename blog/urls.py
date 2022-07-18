05459a0c224770bd7218418d89342c280c9c31de
Samuel Bouye <bouyesamuel@gmail.com>
2022-07-18 15:53:31 +0000

    add boostrap, add base layout

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
     path('<slug:slug>', ArticleDetailView.as_view(), name="article_detail"),
     path('', ArticleListView.as_view(), name="article_list"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
