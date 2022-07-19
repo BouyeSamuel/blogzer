01eca8ec944150382a39ca60c14d5fe84743cfe3
Samuel Bouye <bouyesamuel@gmail.com>
2022-07-19 09:18:06 +0000

    prepare comment form

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, FormView
 from .models import Article
+from .forms import CommentForm
 
 class ArticleListView(ListView):
     model = Article
-    template_name = 'article_list.html'
+    template_name = 'blog/article_list.html'
 
 
 class ArticleDetailView(DetailView):
     model = Article
     template_name = 'blog/article_detail.html'
+
+class CommentFormView(FormView):
+    template_name = 'blog/article_list.html'
+    form_class = CommentForm
+    success_url = '/'
